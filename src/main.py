2ab08c8421439420c4a42f4bd3e496a3f53652fd
gaaiuss <caio.gui.castro@gmail.com>
2026-09-04 14:58:14 -0300

    upd: main.py print

--- src/main.py
+++ src/main.py
@@ -2,9 +2,12 @@ import os
 
 from dotenv import load_dotenv
 from rich import print
+from rich.markdown import Markdown
 
 load_dotenv()
 
 greetings = os.getenv("GREETINGS", "`.env` file not found, check `.env-example`")
 
+print(Markdown("---"))
 print(greetings)
+print(Markdown("---"))
